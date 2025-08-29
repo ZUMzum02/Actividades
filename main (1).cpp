5d2637915a90b08f43e82be922712a3a310cf0e4
ZUMzum02 <paololovon@icloud.com>
2025-08-29 08:31:19 -0500

    cambiar de formato un numero a vertical

---
+++ main (1).cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class numero {
+    public:
+    int a;
+    numero(int x): a(x){}
+    void corroborar(){
+        if ( a <=9999 && a>= 1000){
+            int m = a / 1000;
+            cout << m << endl;
+            int c = (a - m*1000) / 100;
+            cout << c << endl;
+            int d = (a - m*1000 - c*100) /10;
+            cout << d << endl;
+            int s = a-m*1000-c*100-d*10;
+            cout << s << endl;
+        }else {cout <<"solo cuatro digitos pa" << endl;}
+        
+    }
+}; 
+
+int main(){
+    numero* ptr = new numero(1234);
+    ptr->corroborar();
+}
