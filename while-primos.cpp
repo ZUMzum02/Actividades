b7907c5a538dedf3076880e49d9fe8fe5ffdd7f6
ZUMzum02 <paololovon@icloud.com>
2025-09-03 08:20:31 -0500

    Add files via upload

---
+++ while-primos.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    int numero;
+    cout << "ingresa un numero (que sea primo ps): ";
+    cin >> numero;
+    int contador = 0;
+    int divisor = 1;
+    while ( divisor <= numero) {
+        if ( numero % divisor == 1) {
+            contador = contador + 1;
+            }
+        divisor = divisor + 1;
+          
+    }
+     if ( contador == 2) {
+            cout << "es primo bro" << endl;
+            }else {
+                cout << "no es primo bro"<< endl;
+    
+                }
+    
+    
+ return 0;     
+}
