a37e72eab5580bbf03be348de1ba8ae57b567e63
ZUMzum02 <paololovon@icloud.com>
2025-09-03 07:31:05 -0500

    otra formna main (1).cpp

--- main (1).cpp
+++ main (1).cpp
@@ -12,12 +12,19 @@ class numero {
         if ( a <=9999 && a>= 1000){
             int m = a / 1000;
             cout << m << endl;
-            int c = (a - m*1000) / 100;
+            int c = (a - m*1000) / 100; 
             cout << c << endl;
             int d = (a - m*1000 - c*100) /10;
             cout << d << endl;
             int s = a-m*1000-c*100-d*10;
-            cout << s << endl;
+            cout << s << endl;  
+            // ------------------------------------ 
+            cout << "otra forma de hacerlo" << endl;
+            cout << (a / 1000) << endl;
+            cout << (a % 1000) / 100 << endl;
+            cout << (a % 100) / 10 << endl;
+            cout << a & 10 << endl;
+            
         }else {cout <<"solo cuatro digitos pa" << endl;}
         
     }
