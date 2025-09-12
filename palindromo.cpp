e9d38f9f238571072e2d112632a3dde90890aa22
ZUMzum02 <paololovon@icloud.com>
2025-09-12 07:26:37 -0500

    version final

--- palindromo.cpp
+++ palindromo.cpp
@@ -26,17 +26,23 @@ for (int i=0; i < contador/2; i++){
     if (contador / 2 == 1){
             int primer = num / 10;
             int segundo = num - primer*10;
-            ...
+            if (primer == segundo){
+                espalindromo = false;
+                break;
+            }}else{
     int primer = num / (exponencial(10, contador-1-i)) % 10;
     int segundo = num / (exponencial(10, i)) % 10;
     if ( primer != segundo) {
-        cout << "no es palindromo bro" << endl;
         espalindromo = false;
         break;
-    }
+    }}
 }
+
 if (espalindromo == true){
     cout << "es palindromo" <<endl;
+}else{
+cout << "no es palindromo bro" << endl;
 }
 return 0;
 }
+
