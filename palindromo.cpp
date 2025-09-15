a16667010a1a5a0af810fb84552d7114c14e1303
ZUMzum02 <paololovon@icloud.com>
2025-09-15 12:42:55 -0500

    Actualizar palindromo.cpp
    Versión simplicista invirtiendo el número

--- palindromo.cpp
+++ palindromo.cpp
@@ -1,48 +1,31 @@
 #include <iostream>
-
 using namespace std;
-int exponencial (int base, int exponente){
-int resultado = 1;
-for (int i = 0; i < exponente; i++){
-    resultado *= base;
-}
-return resultado;
-}
 
 int main() {
-int num;
-cout << "ingresa tu numero: " << endl;
-cin >> num;
-int contador{0};
-int temp {1};
-
-while (temp < num){
-    temp *= 10;
-    contador += 1;
+    int num, original, invertido = 0, resto;
+    
+    cout << "Ingresa tu numero: ";
+    cin >> num;
+    
+    if (num < 0) {
+        cout << "no es palindromo bro" << endl;
+        return 0;
+    }
+    
+    original = num;
+    
+    // Invertir el número
+    while (num != 0) {
+        resto = num % 10;
+        invertido = invertido * 10 + resto;
+        num /= 10;
+    }
+    
+    if (original == invertido) {
+        cout << "es palindromo" << endl;
+    } else {
+        cout << "no es palindromo bro" << endl;
+    }
+    
+    return 0;
 }
-cout << "largo del numero: " << contador << endl;
-bool espalindromo = true;
-for (int i=0; i < contador/2; i++){
-    if (contador / 2 == 1){
-            int primer = num / 10;
-            int segundo = num - primer*10;
-            if (primer == segundo){
-                espalindromo = false;
-                break;
-            }}else{
-    int primer = num / (exponencial(10, contador-1-i)) % 10;
-    int segundo = num / (exponencial(10, i)) % 10;
-    if ( primer != segundo) {
-        espalindromo = false;
-        break;
-    }}
-}
-
-if (espalindromo == true){
-    cout << "es palindromo" <<endl;
-}else{
-cout << "no es palindromo bro" << endl;
-}
-return 0;
-}
-
